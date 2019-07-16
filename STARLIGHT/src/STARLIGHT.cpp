a26826f8f4c0281b23a448544a2a8d3b47055289
Harper <hrgrimsl@vt.edu>
2019-07-16 09:45:50 -0400

    here

---
+++ STARLIGHT/src/STARLIGHT.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//STARLIGHT Algorithm
+//Takes basis, geometry
+//Returns energy estimate
+int main()
+{
+    for (int r = 0; r<21; ++r) {
+    cout << "*";
+    }
+    cout << "\n";
+    cout << "\"STARLIGHT\" Algorithm\n";
+    cout << "by Harper R. Grimsley\n";
+    cout << "& Nicholas J. Mayhall\n";
+    for (int r = 0; r<21; ++r) {
+    cout << "*";
+    }
+    cout << "\n";
+    //Parse input
+    //Compute SCF reference
+    //Compute 1&2 electron integrals
+    //Characterize all operators
+    //Compute gradients and hessians
+    //Compute energy correction
+    //Return relevant info
+    return 0;
+}
