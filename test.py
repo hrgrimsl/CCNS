fb6787911d7625f55d7a0be957fc3e64dbbe5b94
hrgrimsl <hrgrimsl@vt.edu>
2019-10-03 16:01:07 -0400

    removed rohf test

--- test.py
+++ test.py
@@ -11,12 +11,4 @@ def test_1():
     mol = Molecule(geometry, basis, RHF = True, UNS = False)
     assert abs(mol.conj_grad()+460.17119717)<1e-7
 
-def test_2():
-    geometry = """
-        0 2
-        Cl 0 0 1
-        symmetry c1
-    """
-    basis = 'sto-3g'
-    mol = Molecule(geometry, basis, RHF = False, UNS = True)
-    assert abs(mol.conj_grad()+2.16972360)<1e-7
+
